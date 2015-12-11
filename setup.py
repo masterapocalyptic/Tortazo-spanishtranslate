615f9182a3cc7eae9ad493ec5624fe268a46a550
masterapocalyptic <masterfantasy@openmailbox.org>
2015-12-11 13:57:59 -0600

    se inicia traduccion
    Se inicia con el proceso de traducción al español, se agradece cualquier revisión o corrección de la misma.

--- setup.py
+++ setup.py
@@ -7,18 +7,15 @@ Created on 22/01/2014
 
 Tortazo.py
 
-Tortazo is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation version 2 of the License.
+Tortazo es software libre, puedes editarlo/modificarlo, mejorarlo, traducirlo o distribuirlo
+bajo los terminos indicados por la licencia GNU (GNU General Public License) en su version 2..
 
-Tortazo is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+Tortazo es distribuido con la esperanza de que sea util, sin embargo no se brinda una garantia
+ni siquiera una garantia de COMERCIALIZACION O IDEONIDAD PARA NINGUN FIN. 
+Para mas informacion favor de leer la licencia GNU.
 
-You should have received a copy of the GNU General Public License
-along with Tortazo; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+Deberia de haber recibido una copia de la licencia GNU junto con Tortazo, si no fue asi
+por favor escriba a la Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
 
 import os
@@ -29,8 +26,9 @@ def read(fname):
 
 setup(
     name = "Tortazo",
-    version = "1.1",
+    version = "1.1-es0.1",
     author = "Adastra",
+    traductor="Masterapocalyptic"
     author_email = "debiadastra@gmail.com",
     description = ("Audit and Attack TOR Nodes"),
     license = "GPL",
@@ -39,8 +37,8 @@ setup(
     packages=find_packages(),
     long_description=read('README.md'),
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Audit and Hacking",
+        "Estado de Desarrollo :: 3 - Alpha",
+        "Topic :: Auditoria y hacking",
         "License :: GNU/GPL v2",
     ],
 )
